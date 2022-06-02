3078f85e46f41b1751387be309e8e43d06b5e0d9
honeyai <contact.jessicasanchez@gmail.com>
2022-06-02 10:39:50 -0400

    lil problem with a function in a promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,22 @@
-// import covidNowKey from "../../apiKeys.json";
-
+let key;
+let data;
 const searchBtn = document.querySelector("#search-button");
 
 searchBtn.addEventListener("click", () => {
   getChecked();
+  getCovidKey().then(res => {
+    key = res.covidNowKey;
+    console.log("key", key);
+    getCovidData(key).then(res => console.log(res));
+  })
 });
+
+const getCovidKey = () => fetch("../../apiKeys.json").then(res => res.json());
+
+const getCovidData = (apiKey, place, type) => {
+  fetch(`https://api.covidactnow.org/v2/states.json?apiKey=${apiKey}`).then(res => res.json());
+}
+
 
 //getChecked returns an ARRAY of filters to narrow what displays on the table
 const getChecked = () => {
@@ -12,15 +24,11 @@ const getChecked = () => {
   let marked = document.getElementsByName("filter");
   let labels = [];
   //@ m is an individual checkbox elem of ARRAY marked
-    //Each m has an ARRAY of labels of length 1
+  //Each m has an ARRAY of labels of length 1
   for (let m of marked) {
     if (m.checked) {
       labels.push(m.labels[0].innerText.replace(/\s/g, ""))
     }
   }
   console.log(labels);
-}
-
-const getCovidData = (place) => {
-  fetch("")
 }
