cec78039ab39dee5a6ab5a0a52afe31da731fb0f
Brian <73gs514@gmail.com>
2022-06-02 21:34:00 -0400

    Restrictions for search added

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,18 +23,16 @@ fetch('https://api.github.com/users')
 let autocomplete;
 function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
+    componentRestrictions: {'country' ['US']}
     autocomplete.addListener('place_changed', onPlaceChanged);
 }
 
 function onPlaceChanged() {
     var place = autocomplete.getPlace();
     if (!place.geometry) {
-        document.getElementById('autocomplete').placeholder = 
-        'Enter a place';
-    } else {
-        // document.getElementById('details').innerHTML = place.name;
-        console.log(place)
-    }
+        document.getElementById('autocomplete').placeholder = 'Enter a place';
+    } else 
+        {console.log(place)}
     
 }
 
