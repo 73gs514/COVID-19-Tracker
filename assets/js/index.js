421d2dd4acc772364f8c4b947c4bfc48d643fd23
honeyai <contact.jessicasanchez@gmail.com>
2022-06-02 23:56:11 -0400

    completed the dynamic table. Have to finish if statement to check if there's only one obj. Have to fix from of the labels so that they match the api properties

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ let labels = [];
 const searchBtn = document.querySelector("#search-button");
 const filterBtn = document.querySelector(".filterBtn");
 const filterOptions = document.querySelector(".filterOptions")
-const table = document.querySelector(".table");
+const table = document.querySelector(".results-table");
 
 //returns the covid data in full
 //will give this inform to function that makes a table
@@ -21,7 +21,7 @@ function getCovidData(apiKey, place) {
 
 function getCovidKey() {
   fetch("../../apiKeys.json")
-    .then(res =>  res.json())
+    .then(res => res.json())
     .then(res => getCovidData(res.covidNowKey));
 };
 
@@ -48,7 +48,6 @@ const getChecked = () => {
 
 function toggleFilter(event) {
   let element = event.target;
-  let filterGroup = document.querySelector(".filterOptions");
   if (element.matches(".filterBtn")) {
     if (filterOptions.classList.contains("hidden")) {
       filterOptions.classList.replace("hidden", "visible");
@@ -66,28 +65,55 @@ filterBtn.addEventListener("click", function (e) {
 //@data ARRAY of OBJs
 function makeTables(data, filters) {
   console.log("makeTables", data)
+  let tableBody = document.createElement("tbody");
   //If filters is empty make a table with all of the data
   if (filterBtn.length === 0) {
     for (d of data) {
       //make the rows and columns
+      console.log("d.actuals", d.actuals);
+      let row = document.createElement("tr");
+      let cell = document.createElement("td");
+      let cellText = document.createTextNode(d.actuals);
     }
   } else {
     //check if data is iterable
     console.log(data.length, filters);
+    let headText = document.createTextNode("State");
+    let thead = document.createElement("th");
+    thead.appendChild(headText);
+    table.appendChild(thead);
+    for (let i = 0; i < filters.length; i++) {
+      let headText = document.createTextNode(filters[i]);
+      let thead = document.createElement("th");
+      console.log("headText", headText);
+      thead.appendChild(headText);
+      table.appendChild(thead);
+    }
     for (d of data) {
       console.log("for loop", d);
-      // let row = document.element("tr");
+      let row = document.createElement("tr");
+      let cell = document.createElement("td");
+      let cellText = document.createTextNode(d.state);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
       for (f of filters) {
         let propName = f.charAt(0).toLowerCase() + f.substring(1);
         console.log("word", propName);
         if (d.actuals[propName]) {
           console.log("found", d.actuals[propName]);
           //make a table row
+          let cell = document.createElement("td");
+          let cellText = document.createTextNode(d.actuals[propName]);
+          cell.appendChild(cellText);
+          row.appendChild(cell);
         } else {
           console.log("not found");
         }
       }
+      tableBody.appendChild(row);
     }
+    table.appendChild(tableBody);
+    table.setAttribute("border", "2");
   }
   //for the columns take the length of filters
   //for the rows, make a check to only read the data that has the words that filters has.
